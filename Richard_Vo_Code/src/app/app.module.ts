cc9fca108416e5811a6757d264aa92d473c305c6
Richard Vo <richard.vobusiness@gmail.com>
2018-05-02 19:31:09 -0400

    second push

--- Richard_Vo_Code/src/app/app.module.ts
+++ Richard_Vo_Code/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AboutComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule
