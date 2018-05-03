b9042f3abd65c1c6ae3b728ab2017e1cd13a88d1
alexpich <pich.alex01@gmail.com>
2018-05-03 14:43:17 -0400

    add modal to help page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+// Components
 import { AppComponent } from './app.component';
-
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HelpComponent } from './components/help/help.component';
+
+// Routes
 import {routes} from './routes/routes';
-import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
@@ -28,6 +32,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
