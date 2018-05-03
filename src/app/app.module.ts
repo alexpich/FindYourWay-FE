ae1c633dd9510b186ad12cdd5ff92a8f2b6db84a
Richard Vo <richard.vobusiness@gmail.com>
2018-05-03 13:41:41 -0400

    pushing to my branch

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
+import {routes} from './routes/routes';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AboutComponent,
+    HelpComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
