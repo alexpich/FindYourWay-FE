6e8a50ec60b78cbada236695aab67c598aa66b2b
Richard Vo <richard.vobusiness@gmail.com>
2018-05-04 22:08:46 -0400

    services+environment changes

---
+++ src/app/services/google-services.ts
@@ -0,0 +1,8 @@
+import { environment } from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../models/user';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class GoogleServices{}
