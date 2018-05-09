0d224d68488b8437f33768cf9331375fb004e972
Dimitrei <dnazi@my.apsu.edu>
2018-05-08 22:09:18 -0400

    Update Mapping Component

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -18,23 +18,23 @@ export class CarouselComponent implements OnInit {
   counter = 0;
 
   ngOnInit() {
-    this.image =  document.getElementById('random') as HTMLImageElement;
-    this.image.src = this.images[0];
+    // this.image =  document.getElementById('random') as HTMLImageElement;
+    // this.image.src = this.images[0];
 
-    setInterval(() => {
-      this.nextImage(); }, 4000);
-    }
+    // setInterval(() => {
+    //   this.nextImage(); }, 4000);
+    // }
 
-    nextImage() {
+    // nextImage() {
 
-      if (this.counter < 2) {
-      this.counter++;
-      } else {
-        this.counter = 0;
-      }
+    //   if (this.counter < 2) {
+    //   this.counter++;
+    //   } else {
+    //     this.counter = 0;
+    //   }
 
-      this.image.src = this.images[this.counter];
+    //   this.image.src = this.images[this.counter];
 
-    }
+     }
 
   }
