dcf00b868dda018ba78da7cb0fa61a64eef89503
alexpich <pich.alex01@gmail.com>
2018-05-10 00:04:16 -0400

    removing apiUrl2 for now

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://findyourway-env.7cxshhunvk.us-east-1.elasticbeanstalk.com/',
-  apiUrl2: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+  production: true,
+  apiUrl: 'http://findyourway-env.7cxshhunvk.us-east-1.elasticbeanstalk.com/'
 };
