a4e21def450afce98f13aef95ab4f886e44655bf
Richard Vo <richard.vobusiness@gmail.com>
2018-05-10 02:06:06 -0400

    commit

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -34,13 +34,9 @@ import { QuestionACAComponent } from '../components/question-a-c-a/question-a-c-
 import { QuestionACBComponent } from '../components/question-a-c-b/question-a-c-b.component';
 import { QuestionACCComponent } from '../components/question-a-c-c/question-a-c-c.component';
 import { UpdateuserComponent } from '../components/updateuser/updateuser.component';
-
 import { HomeComponent } from '../components/home/home.component';
-
 import { CouponComponent } from '../components/coupon/coupon.component';
 import { AdminPageComponent } from '../components/admin-page/admin-page.component';
-
-
 
 
 export const routes: Routes = [
